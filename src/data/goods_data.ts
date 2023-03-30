ee7c5f26a8141a7cf7c17f2e0af9cf7d882c00da
Yui <529247970@qq.com>
2023-03-30 22:01:20 +0800

    新增物品列表

---
+++ src/data/goods_data.ts
@@ -0,0 +1,44 @@
+/**
+ * 物品列表
+ * Author：Yui
+ * Create time：2023年03月30日
+ * Modified on：2023年03月30日
+ */
+import { GDData } from "./goods_type"
+const GD00001: GDData = {
+    id: 1,
+    name: '灵石',
+    type: 0,
+    price: 1,
+    sell_price: 1,
+    level: 0,
+    desc: '基础货币，还可以用来修炼'
+}
+const GD00002: GDData = {
+    id: 1,
+    name: '中品灵石',
+    type: 0,
+    price: 100,
+    sell_price: 100,
+    level: 1,
+    desc: '中品灵石，比灵石稀有，100灵石可兑换1中品灵石'
+}
+const GD00003: GDData = {
+    id: 1,
+    name: '上品灵石',
+    type: 0,
+    price: 10000,
+    sell_price: 10000,
+    level: 1,
+    desc: '上品灵石，比中品灵石稀有，100中品灵石可兑换1上品灵石'
+}
+const GD0004: GDData = {
+    id: 1,
+    name: '极品灵石',
+    type: 0,
+    price: 1000000,
+    sell_price: 1000000,
+    level: 1,
+    desc: '极品灵石，非常稀有，100上品灵石可兑换1极品灵石'
+}
+
